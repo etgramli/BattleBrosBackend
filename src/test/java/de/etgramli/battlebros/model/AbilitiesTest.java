9ca8afadc5d7c0ff5450e6fed94ff2222d2dca2c
gitbough <joshua.gramlich@web.de>
2023-01-30 12:32:09 +0100

    added test class for testing abilities of bros

---
+++ src/test/java/de/etgramli/battlebros/model/AbilitiesTest.java
@@ -0,0 +1,13 @@
+package de.etgramli.battlebros.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AbilitiesTest {
+
+    @Test
+    void testAusbrecher() {
+        //todo
+    }
+
+}
